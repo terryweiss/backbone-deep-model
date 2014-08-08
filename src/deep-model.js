65ac9775d5001e53f5bc7d6ee090e33d28f719ac
Terry Weiss <terry.weiss@gmail.com>
2014-08-08 15:23:34 -0400

    deepextend removed

--- src/deep-model.js
+++ src/deep-model.js
@@ -135,7 +135,7 @@
             if (defaults = _.result(this, 'defaults')) {
                 //<custom code>
                 // Replaced the call to _.defaults with _.deepExtend.
-                attrs = _.deepExtend({}, defaults, attrs);
+                attrs = _.defaults({}, defaults, attrs);
                 //</custom code>
             }
             this.set(attrs, options);
